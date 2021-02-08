43399f49fde84c44ab06be3a355d580d173da4da
ShinkuKira21 <crazygaming055@gmail.com>
2021-02-08 14:21:49 +0000

    Added Default Grid Test
    Changed Default Grid Size to:
    
    Width: 6
    Height: 7
    
    Tested Default Grid

--- C4-Theory/xUT/xUT-C4.cs
+++ C4-Theory/xUT/xUT-C4.cs
@@ -55,23 +55,122 @@ namespace xUT
             Assert.Equal(IdealGrid(), grid.OutputGrid());
         }
 
+        [Fact]
+        public void DefaultGridTest()
+        {
+            play = new Players(names, 'X');
+            grid = new Grid(ref play);
+
+            Assert.Equal(DefaultGrid(), grid.OutputGrid());
+        }
+
+        [Fact]
+        public void GridMoveTest()
+        {
+            play = new Players(names, 'X');
+            grid = new Grid(ref play, 4, gridIcon: '-');
+
+            grid.MakeMove(0, 2);
+            grid.MakeMove(1, 2);
+           
+            Assert.NotEqual(IdealGrid(), grid.OutputGrid());
+        }
+
+        [Fact]
+        public void GridValidation1()
+        {
+            // Check if pieces overlap 
+            // Compares two different grids with same actions
+            /* justification:
+             * If the pieces would overlap each other,
+             * Stacking won't exist and test would fail.
+             * 
+             * If pieces were places outside the array limits,
+             * then a memory error will occur, which doesn't.
+             */
+
+            play = new Players(names, 'X');
+            Grid copyGrid = new Grid(ref play, 4);
+            grid = new Grid(ref play, 4);
+
+            copyGrid.MakeMove(0, 2);
+            copyGrid.MakeMove(1, 2);
+            copyGrid.MakeMove(0, 2);
+            copyGrid.MakeMove(1, 2);
+            copyGrid.MakeMove(0, 2);
+
+            grid.MakeMove(0, 2);
+            grid.MakeMove(1, 2);
+            grid.MakeMove(0, 2);
+            grid.MakeMove(1, 2);
+            grid.MakeMove(0, 2);
+
+            Assert.Equal(copyGrid.OutputGrid(), grid.OutputGrid());
+            Assert.Equal(ValidationGridNoOverlap(), grid.OutputGrid());
+        }
+
+        /* Static Visual Representation Grid Expectations */
+
+        string DefaultGrid()
+        {
+            /* EXPECTED OUTPUT:
+             *    1   2   3   4   5   6   
+             * 1  .   .   .   .   .   .
+             * 2  .   .   .   .   .   .
+             * 3  .   .   .   .   .   .
+             * 4  .   .   .   .   .   .   
+             * 5  .   .   .   .   .   .
+             * 6  .   .   .   .   .   .
+             * 7  .   .   .   .   .   .
+             */
+
+            string defaultGrid = "    1   2   3   4   5   6   "
+                + "\n1   .   .   .   .   .   .   "
+                + "\n2   .   .   .   .   .   .   "
+                + "\n3   .   .   .   .   .   .   "
+                + "\n4   .   .   .   .   .   .   "
+                + "\n5   .   .   .   .   .   .   "
+                + "\n6   .   .   .   .   .   .   "
+                + "\n7   .   .   .   .   .   .   \n";
+            return defaultGrid;
+        }
+
         string IdealGrid()
         {
             /* EXPECTED OUTPUT:
-             * 0   1   2   3   
-             * -   -   -   -   
-             * -   -   -   -   
-             * -   -   -   -   
-             * -   -   -   -   
+             *     1   2   3   4   
+             * 1   -   -   -   -   
+             * 2   -   -   -   -   
+             * 3   -   -   -   -   
+             * 4   -   -   -   -   
              */
 
-            string idealGrid = "0   1   2   3   "
-                + "\n-   -   -   -   " 
-                + "\n-   -   -   -   "
-                + "\n-   -   -   -   "
-                + "\n-   -   -   -   \n";
+            string idealGrid = "    1   2   3   4   "
+                + "\n1   -   -   -   -   "
+                + "\n2   -   -   -   -   "
+                + "\n3   -   -   -   -   "
+                + "\n4   -   -   -   -   \n";
 
             return idealGrid;
+        }
+
+        string ValidationGridNoOverlap()
+        {
+            /* EXPECTED OUTPUT:
+             *    1   2   3   4   
+             * 1  .   .   O   .   
+             * 2  .   .   X   .   
+             * 3  .   .   O   .   
+             * 4  .   .   X   .   
+             */
+
+            string validationGrid = "    1   2   3   4   "
+                + "\n1   .   .   O   .   "
+                + "\n2   .   .   X   .   "
+                + "\n3   .   .   O   .   "
+                + "\n4   .   .   X   .   \n";
+
+            return validationGrid;
         }
     }
 }
