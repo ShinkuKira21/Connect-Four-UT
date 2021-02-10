b2d8983c5828bed2c5a56c4af00923fd06348690
ShinkuKira21 <crazygaming055@gmail.com>
2021-02-10 16:48:20 +0000

    Added GameCheck logic.
    Tested (Condition: Minor bugs.)

--- C4-Theory/Connect Four/GameStatus.cs
+++ C4-Theory/Connect Four/GameStatus.cs
@@ -5,17 +5,96 @@ namespace Connect_Four
     public class GameStatus
     {
         protected Players players;
+        protected char[] playerIcons;
         protected Grid grid;
+        public int winnerIndex;
 
         public GameStatus(ref Players players, ref Grid grid)
         {
             this.players = players;
             this.grid = grid;
+
+            playerIcons = new char[2];
+
+            for(int i = 0; i < 2; i++)
+                playerIcons[i] = this.players.GetPlayerIcon(i);
         }
 
-        public bool GetGameStatus(char grid)
-        { return false; }
+        public bool GetGameStatus()
+        { 
+            if ((winnerIndex = CheckGame()) != -1) return false; // false = gameOver
+            else return true; //true = game is not finished 
+        } 
 
-        
+        public int GetWinner()
+        { return winnerIndex; }
+
+        int CheckGame()
+        {
+            int count = 0;
+            char[][] grid = this.grid.GetGrid();
+
+            for(int i = 0; i < 2; i++)
+            {
+                // Check Diagonals 
+                for (int j = 0; j < this.grid.GetXSize(); j++)
+                {
+                    count = 0;
+                    for (int k = 0; k < this.grid.GetYSize(); k++)
+                    {
+                        if (j == k)
+                        {               
+                            if (count == 4)
+                                return i;
+
+                            if (grid[j][k] == playerIcons[i]) count++;
+                            else count = 0;
+                        }
+                    }
+                }
+
+                if (count == 4)
+                    return i;
+
+                    // Check Straight D-U
+                    for (int j = 0; j < this.grid.GetYSize(); j++)
+                    {
+                        count = 0;
+                        for (int k = 0; k < this.grid.GetXSize(); k++)
+                        {  
+                            //Check within loop (Internal)
+                            if (count == 4)
+                                return i;
+
+                            if (grid[k][j] == playerIcons[i]) count++;
+                            else count = 0;
+                        }
+                    }
+
+                // Check after loop (External)
+                if (count == 4)
+                    return i;
+
+                // Check Horizontal L-R
+                for(int j = 0; j < this.grid.GetXSize(); j++)
+                {
+                    for(int k = 0; k < this.grid.GetYSize(); k++)
+                    {               
+                        if (count == 4)
+                            return i;
+
+                        if (grid[j][k] == playerIcons[i]) count++;
+                        else count = 0;
+                    }
+                }
+
+                if (count == 4)
+                    return i;
+            }
+                       
+                        
+
+            return -1; // No Winner Determined
+        }
     }
 }
--- C4-Theory/Connect Four/Program.cs
+++ C4-Theory/Connect Four/Program.cs
@@ -8,11 +8,13 @@ namespace Connect_Four
         { "John", "Mary" };
         protected static Players play;
         protected static Grid grid;
+        protected static GameStatus gs;
 
         static void Main(string[] args)
         {
             PlayerSetup();
             GridSetup();
+            GameSetup();
         }
 
         static void PlayerSetup()
@@ -32,12 +34,70 @@ namespace Connect_Four
 
             grid.DrawGrid();
 
+            // Debug Vertical 
+            //int horizontal = 5;
+            //grid.MakeMove(1, horizontal);
+            //grid.MakeMove(1, horizontal);
+            //grid.MakeMove(1, horizontal);
+            //grid.MakeMove(0, horizontal);
+            //grid.MakeMove(0, horizontal);
+            //grid.MakeMove(0, horizontal);
+            //grid.MakeMove(0, horizontal);
+
+            // Debug Horizontal 
+            //grid.MakeMove(0, 0);
+            //grid.MakeMove(1, 1);
+            //grid.MakeMove(0, 2);
+            //grid.MakeMove(1, 3);
+            //grid.MakeMove(0, 4);
+            //grid.MakeMove(1, 5);
+
+            //grid.MakeMove(0, 0);
+            //grid.MakeMove(0, 1);
+            //grid.MakeMove(1, 2);
+            //grid.MakeMove(1, 3);
+            //grid.MakeMove(1, 4);
+            //grid.MakeMove(1, 5);
+
+            //Debug Horizontal TL-BR
+            grid.MakeMove(0, 5);
+            grid.MakeMove(0, 5);
+            grid.MakeMove(0, 5);
+            grid.MakeMove(1, 5);
+            grid.MakeMove(0, 5);
+            grid.MakeMove(0, 5);
+            grid.MakeMove(0, 5);
+
+            grid.MakeMove(0, 4);
+            grid.MakeMove(0, 4);
+            grid.MakeMove(0, 4);
+            grid.MakeMove(1, 4);
+            grid.MakeMove(1, 4);
+            grid.MakeMove(1, 4);
+
+            grid.MakeMove(1, 3);
+            grid.MakeMove(0, 3);
+            grid.MakeMove(0, 3);
+            grid.MakeMove(1, 3);
+            grid.MakeMove(0, 3);
+
             grid.MakeMove(0, 2);
             grid.MakeMove(1, 2);
-            grid.MakeMove(0, 2);
             grid.MakeMove(1, 2);
+            grid.MakeMove(0, 2);
 
             grid.DrawGrid();
+        }
+
+        static void GameSetup()
+        {
+            gs = new GameStatus(ref play, ref grid);
+
+            if (gs.GetGameStatus())
+                Console.WriteLine("Game Continuing");
+
+            else
+                Console.WriteLine("Game Ended | Winner: " + play.GetPlayerName(gs.GetWinner()));
         }
     }
 }
